fbecab8276a59d9321437becdc585935cdc2589a
walter <walter8098@gmail.com>
2016-01-13 09:35:59 +0800

    添加项目Tracing.Core

---
+++ Tracing.Core/Helpers/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracing.Core
+{
+    public class Util
+    {
+        public static bool Equal(Object a, Object b)
+        {
+            return a == b || (a != null && a.Equals(b));
+        }
+
+        public static IList<T> SortedList<T>(Collection<T> input) where T : IComparable<T>
+        {
+            if (input == null || 0 == input.Count) return new Collection<T>();
+            if (input.Count == 1) return new ReadOnlyCollection<T>(input);
+            List<T> result = input.ToList();
+            result.Sort();
+            return new ReadOnlyCollection<T>(result);
+        }
+
+        public static long GetCurrentTimeStamp()
+        {
+            var t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return Convert.ToInt64(t.TotalMilliseconds * 1000);
+        }
+    }
+}
