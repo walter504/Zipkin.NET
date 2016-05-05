0e0b64f4fea3598729f0e9a4b3b1bee5ffd469be
walter <walter8098@gmail.com>
2016-05-05 17:36:39 +0800

    添加项目Zipkin.Storage.Cassandra

---
+++ Zipkin.Storage.Cassandra/CassandraSpanStore.cs
@@ -0,0 +1,44 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Zipkin.Storage.Cassandra
+{
+    public class CassandraSpanStore
+    {
+        public static readonly String KEYSPACE = "zipkin";
+        public static readonly short BUCKETS = 10;
+
+        private readonly ISession session;
+        private readonly PreparedStatement selectTraces;
+        private readonly PreparedStatement insertSpan;
+        private readonly PreparedStatement selectDependencies;
+        private readonly PreparedStatement insertDependencies;
+        private readonly PreparedStatement selectServiceNames;
+        private readonly PreparedStatement insertServiceName;
+        private readonly PreparedStatement selectSpanNames;
+        private readonly PreparedStatement insertSpanName;
+        private readonly PreparedStatement selectTraceIdsByServiceName;
+        private readonly PreparedStatement insertTraceIdByServiceName;
+        private readonly PreparedStatement selectTraceIdsBySpanName;
+        private readonly PreparedStatement insertTraceIdBySpanName;
+        private readonly PreparedStatement selectTraceIdsByAnnotations;
+        private readonly PreparedStatement insertTraceIdByAnnotation;
+        private readonly PreparedStatement selectTraceIdsBySpanDuration;
+        private readonly PreparedStatement insertTraceIdBySpanDuration;
+
+        public CassandraSpanStore(string keyspace, Cluster cluster, bool ensureSchema)
+        {
+            session = cluster.Connect(keyspace);
+            
+            insertSpan = session.Prepare("");
+
+        }
+
+        public Task Accept(IEnumerable<Span> spans)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
