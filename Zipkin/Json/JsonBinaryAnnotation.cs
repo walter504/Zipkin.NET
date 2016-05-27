198690900f3330d70e84e6f7f5cac371a4f78c4d
walter <walter8098@gmail.com>
2016-05-27 14:18:19 +0800

    修复JsonBinaryAnnotation获取AnnotationType的bug

--- Zipkin/Json/JsonBinaryAnnotation.cs
+++ Zipkin/Json/JsonBinaryAnnotation.cs
@@ -7,7 +7,7 @@ namespace Zipkin.Json
     {
         public string key { get; set; }
         public object value { get; set; }
-        public string annotationType { get; set; }
+        public string type { get; set; }
         public JsonEndpoint endpoint { get; set; }
 
         public JsonBinaryAnnotation()
@@ -17,31 +17,47 @@ namespace Zipkin.Json
         public JsonBinaryAnnotation(BinaryAnnotation ba)
         {
             key = ba.key;
-            annotationType = ba.type.ToString();
+            type = ba.type.ToString();
             endpoint = new JsonEndpoint(ba.endpoint);
             switch(ba.type)
             {
-                case AnnotationType.BOOL: value = BitConverter.ToBoolean(ba.value, 0); break;
+                case AnnotationType.BOOL: type = null; value = BitConverter.ToBoolean(ba.value, 0); break;
                 case AnnotationType.BYTES: value = Convert.ToBase64String(ba.value); break;
                 case AnnotationType.I16: value = BitConverter.ToInt16(ba.value, 0); break;
                 case AnnotationType.I32: value = BitConverter.ToInt32(ba.value, 0); break;
                 case AnnotationType.I64: value = BitConverter.ToInt64(ba.value, 0); break;
                 case AnnotationType.DOUBLE: value = BitConverter.ToDouble(ba.value, 0); break;
-                case AnnotationType.STRING: value = Encoding.UTF8.GetString (ba.value); break;
+                case AnnotationType.STRING: type = null; value = Encoding.UTF8.GetString(ba.value); break;
                 default: throw new Exception(string.Format("Unsupported annotation type: {0}", ba));
             }
         }
 
         public BinaryAnnotation Invert()
         {
-            return new BinaryAnnotation(key, GetByteBuffer(), (AnnotationType)Enum.Parse(typeof(AnnotationType), annotationType), endpoint.Invert());
+            AnnotationType annotationType;
+            if (!Enum.TryParse<AnnotationType>(type, out annotationType))
+            {
+                if (value is String)
+                {
+                    annotationType = AnnotationType.STRING;
+                }
+                else if (value is Boolean)
+                {
+                    annotationType = AnnotationType.BOOL;
+                }
+                else
+                {
+                    throw new Exception(string.Format("Unsupported json annotation type: {0}", this));
+                }
+            }
+            return new BinaryAnnotation(key, GetByteBuffer(annotationType), annotationType, endpoint.Invert());
         }
 
-        private byte[] GetByteBuffer()
+        private byte[] GetByteBuffer(AnnotationType type)
         {
             try
             {
-                switch ((AnnotationType)Enum.Parse(typeof(AnnotationType), annotationType))
+                switch (type)
                 {
                     case AnnotationType.BOOL: return BitConverter.GetBytes((bool)value);
                     case AnnotationType.BYTES: return Convert.FromBase64String((string)value);
@@ -58,6 +74,5 @@ namespace Zipkin.Json
                 throw new Exception(string.Format("Error parsing json binary annotation: {0}", e));
             }
         }
-
     }
 }
